4f9ec5f099afe1be1ce3c137e87db4d1a5a1e0d3
Gustavo Brito <britopereiragustavo@gmail.com>
2020-08-25 23:04:09 -0300

    Starting project structure

--- librarians/core/models.py
+++ librarians/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Tags(models.Model):
+    title = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Matter(models.Model):
+    title = models.CharField(max_length=128)
+    tags = models.ManyToManyField('Tags')
+    contents = models.ForeignKey('Content', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titlep
+
+
+class Content(models.Model):
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    description = models.TextField()
+    # file = models.FileField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
